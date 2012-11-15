d1889e79eac940892127149661077e758e045755
Ryan Huynh <chhuynh@uci.edu>
2012-11-14 19:58:27 -0800

    major change

--- MedApp/MedApp/ViewController.h
+++ MedApp/MedApp/ViewController.h
@@ -14,10 +14,20 @@
 @property (weak, nonatomic) IBOutlet UITextField *heightTextField;
 @property (weak, nonatomic) IBOutlet UITextField *weightTextField;
 @property (weak, nonatomic) IBOutlet UILabel *heightLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *weightLabel;
+
+@property (strong, nonatomic) IBOutlet UIImageView *firstScreen;
+@property (strong, nonatomic) NSMutableDictionary *heightChartB;
+@property (strong, nonatomic) NSMutableDictionary *weightChartB;
+@property (strong, nonatomic) NSMutableDictionary *weightChartG;
+@property (strong, nonatomic) NSMutableDictionary *heightChartG;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *gender;
+
+@property (strong, nonatomic) NSMutableDictionary *weightChartUse;
+@property (strong, nonatomic)NSMutableDictionary *heightChartUse;
 
 - (IBAction)submitButton:(id)sender;
 - (IBAction)milestonesButton:(id)sender;
+- (IBAction)genderChange:(id)sender;
 
 @end
--- MedApp/MedApp/ViewController.m
+++ MedApp/MedApp/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  App2
+//  MedApp
 //
 //  Created by Eiling Fernandez on 11/11/12.
 //  Copyright (c) 2012 UCI. All rights reserved.
@@ -19,7 +19,14 @@
 @synthesize weightTextField;
 @synthesize heightLabel;
 @synthesize weightLabel;
-
+@synthesize weightChartB;
+@synthesize heightChartB;
+@synthesize weightChartG;
+@synthesize heightChartG;
+@synthesize gender;
+@synthesize firstScreen;
+@synthesize weightChartUse;
+@synthesize heightChartUse;
 
 - (void)viewDidLoad
 {
@@ -30,6 +37,90 @@
     self.ageTextFIeld.delegate = self;
     self.weightTextField.delegate = self;
     
+    
+    //Initialize dictionary
+    //Boy Weight chart, variable name is corespoding to age, e.g. bw0 is for boy with age 0, bw1 for boy with age 1, etc..
+    NSMutableDictionary *bw0 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                              @"3rd Percentile for Weight.", @"2.3",
+                              @"5th Percentile for Weight.", @"2.5",
+                              @"10th Percentile for Weight.", @"2.7",
+                              @"25th Percentile for Weight.", @"3.1",
+                              @"50th Percentile for Weight.", @"3.5",
+                              @"75th Percentile for Weight.", @"3.8",
+                              @"90th Percentile for Weight.", @"4.1",
+                              @"95th Percentile for Weight.", @"4.3",
+                              @"97th Percentile for Weight.", @"4.4",
+                                  @"BOY", @"3.6",
+                               nil];
+    
+    NSMutableDictionary *bw1 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                               @"3rd Percentile for Weight.", @"3.6",
+                               @"5th Percentile for Weight.", @"3.7",
+                               @"10th Percentile for Weight.", @"4.0",
+                               @"25th Percentile for Weight.", @"4.4",
+                               @"50th Percentile for Weight.", @"4.8",
+                               @"75th Percentile for Weight.", @"5.3",
+                               @"90th Percentile for Weight.", @"5.7",
+                               @"95th Percentile for Weight.", @"5.9",
+                               @"97th Percentile for Weight.", @"6.1",
+                               nil];
+    
+    
+    weightChartB = [[NSMutableDictionary alloc]initWithObjectsAndKeys:
+                    bw0, [NSNumber numberWithInt:0],
+                    bw1, [NSNumber numberWithInt:1],
+                    nil];
+    
+    //Boy Height Chart.
+    NSMutableDictionary *bh0 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                               @"3rd Percentile for Height.", @"44.9",
+                               @"5th Percentile for Height.", @"45.5",
+                               @"10th Percentile for Height.", @"46.5",
+                               @"25th Percentile for Height.", @"48.1",
+                               @"50th Percentile for Height.", @"49.9",
+                               @"75th Percentile for Height.", @"51.7",
+                               @"90th Percentile for Height.", @"53.3",
+                               @"95th Percentile for Height.", @"54.3",
+                               @"97th Percentile for Height.", @"54.9",
+                               nil];
+    
+    NSMutableDictionary *bh1 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                                @"3rd Percentile for Height.", @"52.1",
+                                @"5th Percentile for Height.", @"52.7",
+                                @"10th Percentile for Height.", @"53.5",
+                                @"25th Percentile for Height.", @"54.9",
+                                @"50th Percentile for Height.", @"56.6",
+                                @"75th Percentile for Height.", @"58.3",
+                                @"90th Percentile for Height.", @"59.9",
+                                @"95th Percentile for Height.", @"60.9",
+                                @"97th Percentile for Height.", @"61.6",
+                                nil];
+
+    
+    
+    heightChartB = [[NSMutableDictionary alloc]initWithObjectsAndKeys:
+                    bh0, [NSNumber numberWithInt:0],
+                    bh1, [NSNumber numberWithInt:1],
+                    nil];
+    
+    //Girl Weight Chart.
+    NSMutableDictionary *gw0 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                                @"3rd Percentile for Height.", @"2.4",
+                                @"5th Percentile for Height.", @"2.5",
+                                @"10th Percentile for Height.", @"2.7",
+                                @"25th Percentile for Height.", @"3.1",
+                                @"50th Percentile for Height.", @"3.4",
+                                @"75th Percentile for Height.", @"3.7",
+                                @"90th Percentile for Height.", @"3.9",
+                                @"95th Percentile for Height.", @"4.1",
+                                @"97th Percentile for Height.", @"4.2",
+                                 @"GIRL", @"3.6",
+                                nil];
+    weightChartG = [[NSMutableDictionary alloc]initWithObjectsAndKeys:
+                    gw0, [NSNumber numberWithInt:0],
+                                       nil];
+
+
 }
 
 - (void)viewDidUnload
@@ -39,6 +130,8 @@
     [self setHeightTextField:nil];
     [self setAgeTextFIeld:nil];
     [self setWeightTextField:nil];
+    
+    //NEED TO DESTROY DICTIONARIES LATER.
     [super viewDidUnload];
 }
 
@@ -133,16 +226,69 @@ void submitAll(UITextField *ageTextFIeld, UITextField *heightTextField, UILabel 
         }
         
     }
-}
+    
+    }
 
 - (IBAction)submitButton:(id)sender {
     
-    submitAll(ageTextFIeld, heightTextField, heightLabel, weightTextField, weightLabel);
+    //submitAll(ageTextFIeld, heightTextField, heightLabel, weightTextField, weightLabel);
+   
+       double distance = 0;
+    double weight = round(weightTextField.text.doubleValue * 0.453592 * 10)/10.0 ;
+    NSMutableDictionary *wTemp = [weightChartUse objectForKey:[NSNumber numberWithInt:ageTextFIeld.text.intValue]];
+    while([wTemp objectForKey:[NSString stringWithFormat:@"%.1f", weight + distance]] == nil
+          &&[wTemp objectForKey:[NSString stringWithFormat:@"%.1f", weight - distance]] == nil)
+    {
+       distance += 0.1;
+    }
+    if(distance == 0)
+        weightLabel.text = [wTemp objectForKey:[NSString stringWithFormat:@"%.1f", weight]];
+    else
+    {
+        if([wTemp objectForKey:[NSString stringWithFormat:@"%.1f", weight + distance]] != nil)
+            weightLabel.text = [wTemp objectForKey:[NSString stringWithFormat:@"%.1f", weight + distance]];
+        else
+            weightLabel.text = [wTemp objectForKey:[NSString stringWithFormat:@"%.1f", weight - distance]];
+    }
+    
+    distance = 0;
+    double height = round(heightTextField.text.doubleValue * 2.54 * 10)/10.0 ;
+    NSMutableDictionary *hTemp = [heightChartB objectForKey:[NSNumber numberWithInt:ageTextFIeld.text.intValue]];
+    while([hTemp objectForKey:[NSString stringWithFormat:@"%.1f", height + distance]] == nil
+          &&[hTemp objectForKey:[NSString stringWithFormat:@"%.1f", height - distance]] == nil)
+    {
+        distance += 0.1;
+    }
+    if(distance == 0)
+        heightLabel.text = [hTemp objectForKey:[NSString stringWithFormat:@"%.1f", height]];
+    else
+    {
+        if([hTemp objectForKey:[NSString stringWithFormat:@"%.1f", height + distance]] != nil)
+            heightLabel.text = [hTemp objectForKey:[NSString stringWithFormat:@"%.1f", height + distance]];
+        else
+            heightLabel.text = [hTemp objectForKey:[NSString stringWithFormat:@"%.1f", height - distance]];
+    }
+
  
     
 }
 
 - (IBAction)milestonesButton:(id)sender {
+}
+
+- (IBAction)genderChange:(id)sender {
+    
+    if(self.gender.selectedSegmentIndex == 0){
+        weightChartUse= weightChartB;
+        heightChartUse= weightChartB;
+        firstScreen.image = [UIImage imageNamed:@"boy.png"];}
+    else{
+        weightChartUse = weightChartG;
+        heightChartUse = weightChartG;
+        firstScreen.image = [UIImage imageNamed:@"girl.png"];}
+    
+ 
+    
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
