0eaf6973f9113cc9a44f93e13a0d14fd8b732043
Eiling Fernandez <eilingfernan@gmail.com>
2012-11-16 22:41:26 -0800

    Friday 10:41 PM

--- MedApp/MedApp/Milestone.h
+++ MedApp/MedApp/Milestone.h
@@ -2,12 +2,27 @@
 //  Milestone.h
 //  MedApp
 //
-//  Created by Ryan Huynh on 11/15/12.
+//  Created by Eiling Fernandez on 11/16/12.
 //  Copyright (c) 2012 Ryan Huynh. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface Milestone : UIViewController
+
+<UIPickerViewDataSource,UIPickerViewDelegate>
+{
+    IBOutlet UIPickerView *agePicker;
+    NSArray *ageData;
+}
+
+@property(nonatomic, retain) NSArray *ageData;
+
+@property (nonatomic,retain) IBOutlet UIPickerView *agePicker;
+@property (weak, nonatomic) IBOutlet UITextField *ageTextField;
+
+- (IBAction)showAge:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *agePickerViewContainer;
+- (IBAction)pickerDone:(id)sender;
 
 @end
--- MedApp/MedApp/Milestone.m
+++ MedApp/MedApp/Milestone.m
@@ -2,7 +2,7 @@
 //  Milestone.m
 //  MedApp
 //
-//  Created by Ryan Huynh on 11/15/12.
+//  Created by Eiling Fernandez on 11/16/12.
 //  Copyright (c) 2012 Ryan Huynh. All rights reserved.
 //
 
@@ -13,6 +13,14 @@
 @end
 
 @implementation Milestone
+
+@synthesize agePicker;
+@synthesize ageData;
+@synthesize ageTextField;
+@synthesize agePickerViewContainer;
+
+
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,7 +34,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+	// Do any additional setup after loading the view.
+    
+    ageData = [[NSArray alloc] initWithObjects: @"0", @"1",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12", nil];
+    agePickerViewContainer.hidden = YES;
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    ageTextField.text = [ageData objectAtIndex:[pickerView selectedRowInComponent:0]];
+}
+
+-(NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return [ageData count];
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return [ageData objectAtIndex:row];
+}
+
+
+- (IBAction)showAge:(id)sender {
+    agePickerViewContainer.hidden = NO;
+}
+
+- (IBAction)pickerDone:(id)sender {
+    agePickerViewContainer.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +70,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
